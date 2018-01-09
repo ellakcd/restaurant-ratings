a7de937a5722ffa2c48b596d07c1221f13af71c2
Hackbright Student <no-reply@hackbrightacademy.com>
2018-01-08 17:24:07 -0800

    add user interaction function

--- ratings.py
+++ ratings.py
@@ -31,6 +31,7 @@ def organize_restaurant_ratings(file_name):
 
 def print_sorted(restaurant_ratings):
     """Print restaurants in alphabetical order with rating."""
+
     print "--- Restaurants and Ratings ---"
     for restaurant, rating in sorted(restaurant_ratings.items()):
         print "%s is rated at %s" % (restaurant, rating)
@@ -38,6 +39,7 @@ def print_sorted(restaurant_ratings):
 
 def get_new_restaurant():
     """Asks user for new restaurant and ratings; adds it to dictionary."""
+
     print "Add a restaurant and rating to the list."
     new_restaurant = raw_input("Give me a restaurant! ")
     new_rating = raw_input("What's its score? ")
@@ -48,24 +50,28 @@ def get_new_restaurant():
     except ValueError:
         print "That's not a number!"
         get_new_restaurant()
+
     restaurant_and_ratings[new_restaurant] = new_rating
 
     print_sorted(restaurant_and_ratings)
 
 
-restaurant_and_ratings = organize_restaurant_ratings(argv[1])
-while True:
-    print "\nTo see all ratings in alphabetical order type 'A'."
-    print "To add and rate a new restaurant type 'B'."
-    print "To quit, type 'Q'"
-    behavior = raw_input(">> ").lower()
+def get_user_interaction():
+    while True:
+        print "\nTo see all ratings in alphabetical order type 'A'."
+        print "To add and rate a new restaurant type 'B'."
+        print "To quit, type 'Q'"
+        behavior = raw_input(">> ").lower()
 
-    if behavior == 'a':
-        print_sorted(restaurant_and_ratings)
-    elif behavior == 'b':
-        get_new_restaurant()
-    elif behavior == 'q':
-        quit()
-    else:
-        print "I didn't understand that!"
-        print "Please type 'A', 'B', or 'Q'."
+        if behavior == 'a':
+            print_sorted(restaurant_and_ratings)
+        elif behavior == 'b':
+            get_new_restaurant()
+        elif behavior == 'q':
+            quit()
+        else:
+            print "I didn't understand that!"
+            print "Please type 'A', 'B', or 'Q'."
+
+restaurant_and_ratings = organize_restaurant_ratings(argv[1])
+get_user_interaction()
